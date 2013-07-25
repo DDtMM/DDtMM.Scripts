c053035bdbad4ce82727fbadbb8f86846c4a8de1
Daniel Gimenez <danny@gimenez.biz>
2013-07-25 15:03:59 -0400

    Updated Transimate and created matrix

---
+++ DDtMM.Scripts/Scripts/DataStructures/dgMatrtix-0-1-0.js
@@ -0,0 +1,395 @@
+﻿var Matrix = (function () {
+    function isNumber(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
+    function isString(str) {
+        return ( Object.prototype.toString.call(str) == '[object String]' ) ;
+    }
+    function isMatrix(obj) {
+        return obj.constructor == Matrix;
+    }
+    // Matrix constructor
+    // source: based on type the following happens
+    //   Matrix: then this is created copying values in source
+    //   Number: then a square matrix with 0 values is created, so if 2 passed then 2x2 is created
+    //   Array: 
+    //     if 2 dimensional array (array of arrays), then the first dimension is rows, and second is columns.
+    //        assumes that column arrays are of equal length.
+    //     if 1 dimensional and options.rows and options.cols passed then rows x cols = array.length
+    //     else create a square array
+    //   String: attempts to parse the string.  If nothing is passed to options.rowbreak, then a row
+    //     is terminated with a new line.
+    // options: object literal with various options for a source
+    //   .rows: number of rows if array is passed as source.  If source is a string, is used and cols in computed
+    //   .cols: number of columns if array is passed as source.  If source is a string, is used and rows in computed
+    //   .rowbreak: either a regular expression or string that ends rows if a string is passed as source
+    //   .isSquare: creates a square matrix if string is passed as source
+    var Matrix = function (source, options) {
+        this.array = [];
+        this.rows = 0;
+        this.cols = 0;
+
+        // if source is a Matrix then copy values.
+        if (isMatrix(source.constructor)) {
+            this.rows = source.rows;
+            this.cols = source.cols;
+            this._initArray();
+            for (var i = 0, il = this.array.length; i < il; i++) {
+                this.array[i] = source.array[i];
+            }
+        }
+        else if (source.rows !== undefined && source.cols !== undefined) {
+            this.rows = source.rows;
+            this.cols = source.cols;
+            this.init();
+        }
+        else if (isNumber(source)) {
+            // assumes square matrix
+            this.rows = this.cols = source;
+            this.init();
+        }
+        else if (Array.isArray(source)) {
+            if (source.length == 0) {
+                throw ('invalid length for array');
+            }
+            else if (Array.isArray(source[0])) {
+                // a 2d array
+                this.rows = source.length;
+                this.cols = source[0].length;
+                this._initArray();
+
+                for (var i = 0; i < this.rows; i++) {
+                    for (var j = 0; j < this.cols; i++) {
+                        this.array[i * this.cols + j] = source[i][j];
+                    }
+                }
+            } else {
+                if (options && options.rows && options.cols) {
+                    this.rows = options.rows;
+                    this.cols = options.cols;
+                } else {
+                    // assumes square matrix;
+                    this.rows = this.cols = Math.sqrt(source.length);
+                }
+                this._initArray();
+                if (this.array.length != source.length) {
+                    throw ('source array does not not jive with rows and cols');
+                }
+                for (var i = 0, il = this.array.length; i < il; i++) {
+                    this.array[i] = source[i];
+                }
+            }
+        }
+        else if (isString(source)) {
+            var match,
+                arrayLen,
+                size,
+                reNumbers = /[-+]?\d*\.?\d+/g,
+                reBreaks = /[\n\r]+/g
+            ;
+            this.array = [];
+            while ((match = reNumbers.exec(source))) {
+                this.array.push(parseFloat(match));
+            }
+            arrayLen = this.array.length;
+
+ 
+            if (options && options.isSquare) {
+                size = Math.sqrt(arrayLen);
+                if (size == Math.floor(size)) {
+                    this.rows = this.cols = size;
+                }
+                else {
+                    throw 'not a square matrix.';
+                }
+            }
+            else if (options && options.cols) {
+                this.rows = arrayLen / options.cols;
+                this.cols = options.cols;
+            }
+            else if (options && options.rows) {
+                this.cols = arrayLen / options.rows;
+                this.rows = options.rows;
+            }
+            else {
+                // use breaks to find rows
+                if (options && options.rowbreak) {
+                    if (options.rowbreak.constructor == RegExp) {
+                        reBreaks = options.rowbreak;
+                        if (!reBreaks.global) throw 'global required for rowbreak RegExp object';
+                    }
+                    else {
+                        reBreaks = new RegExp(options.rowbreak, "g");
+                    }
+                }
+
+                size = 1;
+                while ((match = reBreaks.exec(source.trim()))) {
+                    size++;
+                }
+                this.cols = arrayLen / size;
+                if (this.cols == Math.floor(this.cols)) {
+                    this.rows = size;
+                }
+                else {
+
+                    throw ('unable to determine dimensions from string');
+                }
+            }
+        }
+        else {
+            throw ('Either size, array (for square matrix) Matrix, string, or { rows, cols } required for constructor');
+        }
+
+        this.isSquare = true;
+    };
+
+    (function () {
+        // in order of columns then rows [r1, c1] [r1, c2] ...
+        this.array = [];
+        this.rows = 0;
+        this.cols = 0;
+        this.isSquare = false;
+
+        // creates a new array of the size of rows and cols.
+        this._initArray = function() {
+            this.array = new Array(this.rows * this.cols);
+        }
+
+        // creates the value to do the operation on.  If value is a matrix, just returns value.
+        this._createOperatingMatrix = function(value) {
+            if (isMatrix(value)) return value;
+            return new Matrix(value);
+        }
+
+        // a new matrix that has the minimum rows and cols of this and matrixB
+        this._createMinimumMatrix = function(matrixB) {
+            return new Matrix({ 
+                rows: Math.min(this.rows, matrixB.rows), 
+                cols: Math.min(this.cols, matrixB.cols) 
+            });
+        }
+
+        // calls a scalar or matrix function based on value type
+        this._branchScalarOrMatrix = function(value, scalarFunc, matrixFunc) {
+            if (isNumber(value)) return scalarFunc(value);
+            else return matrixFunc.call(this, this._createOperatingMatrix(value));
+        }
+
+        // performs a function on each value of the matrix.
+        // func = Function (matrixValue, scalarValue);
+        // returns new matrix with result of function
+        this._scalarFuncOnMatrix = function (scalarValue, func) {
+            var result = new Matrix(this);
+
+            for (var i = 0, il = result.array.length; i < il; i++) {
+                result.array[i] = func(result.array[i], scalarValue);
+            }
+
+            return result;
+        }
+        // performs an entrywise function (this and matrixB rows = rows and cols = cols)
+        // func = Function (matrixValue, matrixBvalue);
+        // returns new matrix with result of function
+        this._entryWiseFuncOnMatrix = function (matrixB, func) {
+            if (this.cols != matrixB.cols || this.rows != matrixB.rows) throw ('matrix row col mismatch');
+
+            var result = this._createMinimumMatrix({ cols: this.cols, rows: this.rows }),
+                value;
+
+            for (var i = 0, il = result.array.length; i < il; i++) {
+                result.array[i] = func(this.array[i], matrixB.array[i]);
+            }
+            
+            return result;
+        }
+
+        // performs an function where the result is the value of that function on the rows and cols added together
+        // func = Function (matrixValue, matrixBvalue);
+        // returns new matrix with result of function
+        this._rowColFuncOnMatrix = function (matrixB, func) {
+            if (this.cols != matrixB.rows || this.rows != matrixB.cols) throw ('matrix mismatch: rows != columns');
+
+            var result = new Matrix({ rows: this.rows, cols: matrixB.cols }),
+                value;
+
+            for (var i = 0, il = result.rows; i < il; i++) {
+                for (var j = 0, jl = result.cols; j < jl; j++) {
+                    value = 0;
+                    for (var k = 0; k < this.cols; k++) {
+                        value += func(this.array[this.rows * i + k],
+                            matrixB.array[matrixB.rows * k + j]);
+                    }
+                    result.array[result.rows * i + j] = value;
+                }
+            }
+
+            return result;
+        }
+
+        // sets all values in the matrix to 0
+        this.init = function () {
+            this._initArray();
+            for (var i = 0, il = this.array.length; i < il; i++) {
+                this.array[i] = 0.0;
+            }
+        };
+
+        // gets an array of values for a row
+        this.getRow = function (rowIndex) {
+            var startIndex = rowIndex * this.rows;
+            return this.array.slice(startIndex, startIndex + this.rows + 1);
+        }
+
+        // gets an array of values for a column
+        this.getCol = function (colIndex) {
+            var column = new Array(this.cols);
+            for (var i = 0; i < this.rows; i++) {
+                column[i] = this.array[i * this.rows + colIndex];
+            }
+        }
+
+        // gets a value at a particular row, column.
+        this.get = function (rowIndex, colIndex) {
+            return this.array[this.cols * rowIndex + colIndex];
+        }
+
+        // sets a value at a particular row, column.
+        this.set = function (rowIndex, colIndex, value) {
+            return this.array[this.cols * rowIndex + colIndex] = value;
+        }
+
+        // is this equal to matrixB?
+        this.equals = function (matrixB) {
+            if (this.cols != matrixB.cols && this.rows != matrixB.rows) return false;
+
+            for (var i = 0, il = this.array.length; i < il; i++) {
+                if (this.array[i] != matrixB.array[i]) return false;
+            }
+
+            return true;
+        }
+
+        // returns identity for this matrix if square.  Also returns identity of given size.
+        this.identity = function (size) {
+            if (size === undefined) {
+                if (this.isSquare) {
+                    size = this.rows;
+                }
+                else {
+                    throw ('can not create identity on non square matrix');
+                }
+            }
+
+            var identity = new Matrix(size);
+            for (var i = 0; i < size; i++) {
+                identity[i * size + i] = 1.0;
+            }
+
+            return identity;
+        };
+
+        // rotates the values in the matrix clockwise
+        this.rotate = function () {
+            var rotated = new Matrix({ rows: this.cols, cols: this.rows });
+            var maxRowIndex = (this.rows - 1) * this.cols;
+            for (var i = 0; i < this.rows; i++) {
+                for (var j = 0; j < this.cols; j++) {
+                    rotated.array[j * this.rows + this.rows - i - 1] = this.array[this.cols * i + j];
+                }
+            }
+            return rotated;
+        }
+
+        // rotates the values in the matrix counter clockwise
+        this.rotateCounter = function () {
+            var rotated = new Matrix({ rows: this.cols, cols: this.rows });
+            var maxRowIndex = (this.rows - 1) * this.cols;
+            for (var i = 0; i < this.rows; i++) {
+                for (var j = 0; j < this.cols; j++) {
+                    rotated.array[(this.cols - j - 1) * this.rows + i] = this.array[this.cols * i + j];
+                }
+            }
+            return rotated;
+        }
+
+        /************
+        multiply 
+        ************/
+        // multiples by either a number or matrix
+        this.multiply = function (multiplier) {
+            return this._branchScalarOrMatrix(multiplier, this.multiplyScalar, this.multiplyMatrix);
+        };
+
+        // multiples each value in the matrix
+        this._multiply = function (a, b) { return a * b; }
+
+        // scalar multiplication
+        this.multiplyScalar = function (multiplier) {
+            return this._scalarFuncOnMatrix(multiplier, this._multiply);
+        }
+
+        // matrix multiplication
+        this.multiplyMatrix = function (multiplier) {
+            return this._rowColFuncOnMatrix(multiplier, this._multiply);
+        }
+        
+        /************
+        addition 
+        ************/
+        // add either a number or matrix
+        this.add = function (addend) {
+            return this._branchScalarOrMatrix(addend, this.addScalar, this.addMatrix);
+        };
+
+        // adds each value in the matrix
+        this._add = function (a, b) { return a + b; }
+
+        // scalar addition
+        this.addScalar = function (addend) {
+            return this._scalarFuncOnMatrix(addend, this._add);
+        }
+
+        // matrix addition
+        this.addMatrix = function (addend) {
+            return this._entryWiseFuncOnMatrix(addend, this._add);
+        }
+
+
+        /************
+        subtraction 
+        ************/
+        // subtracts either a number of matrix
+        this.sub = function (subtrahend) {
+            return this._branchScalarOrMatrix(subtrahend, this.subScalar, this.subMatrix);
+        };
+
+        // subtracs each value in the matrix
+        this._sub = function (a, b) { return a - b; }
+
+        // scalar subtraction
+        this.subScalar = function (subtrahend) {
+            return this._scalarFuncOnMatrix(subtrahend, this._sub);
+        }
+
+        // matrix subtraction
+        this.subMatrix = function (subtrahend) {
+            return this._entryWiseFuncOnMatrix(subtrahend, this._sub);
+        }
+        
+        // string representation of matrix.
+        this.toString = function () {
+            var result = '';
+            for (var i = 0, il = this.array.length; i < il; i += this.cols) {
+                for (var j = 0; j < this.cols; j++) {
+                    result += this.array[i + j] + ', '
+                }
+                result = result.substring(0, result.length - 2) + '\r\n';
+            }
+
+            return result;
+        }
+    }).call(Matrix.prototype);
+
+    return Matrix;
+})();
---
+++ DDtMM.Scripts/Scripts/Plugins/dgTransimate-0.0.4.js
@@ -0,0 +1,376 @@
+﻿(function ($) {
+    var cssPrefix = '',
+		stylePrefix = '',
+		cssTransformKey = 'transform',
+		styleTransformKey = 'transform',
+        propKeys = { transformOrigin: 'transformOrigin' },
+		reFindTransforms = /(\w+)\s*\(([^)]*)\)/g,
+		reParametersSplit = /\s*,\s*/g,
+		nameTransformCache = {};
+
+	// make sure cross browser...
+	// I don't know if this is nescessary.  Every browser I tried worked on the first options.
+	(function () {
+	    var $test = $('DIV');
+	    var elem = $test[0];
+	    
+	    if (elem.style['-moz-transform'] !== undefined) { cssPrefix = '-moz-'; stylePrefix = 'Moz'; }
+	    else if (elem.style['-ms-transform'] !== undefined) { cssPrefix = '-ms-'; stylePrefix = 'ms'; }
+	    else if (elem.style['-webkit-transform'] !== undefined) { cssPrefix = '-webkit-'; stylePrefix = 'Webkit'; }
+	    else if (elem.style['-webkit-transform'] !== undefined) { cssPrefix = '-o-'; stylePrefix = 'o'; }
+	    else if (elem.style['transform'] !== undefined) { cssPrefix = ''; stylePrefix = ''; }
+		else {
+			throw ('unable to detect set/get methods for transform style.');
+		}
+		if (cssPrefix) {
+			cssTransformKey = cssPrefix + cssTransformKey;
+			styleTransformKey = stylePrefix + 'Transform';
+		    // create other keys
+			var keyValue;
+			for (var i in propKeys) {
+			    keyValue = propKeys[i];
+			    propKeys[i] = stylePrefix + keyValue[0].toUpperCase() + keyValue.substring(1, keyValue.length);
+			}
+		}
+
+		$.cssHooks.transformOrigin = {
+		    set: function (elem, val, unit) {
+		        if (!$.support.matrixFilter) {
+		            val = (typeof val === 'string') ? val : val + (unit || '%');
+		            elem.style[$.cssProps.transformOrigin] = val;
+		        } else {
+		            val = val.split(",");
+		            $.cssHooks.transformOriginX.set(elem, val[0]);
+		            if (val.length > 1) {
+		                $.cssHooks.transformOriginY.set(elem, val[1]);
+		            }
+		        }
+		    },
+		    get: function (elem, computed) {
+		        if (!$.support.matrixFilter) {
+		            return elem.style[$.cssProps.transformOrigin];
+		        } else {
+		            var originX = $.data(elem, 'transformOriginX');
+		            var originY = $.data(elem, 'transformOriginY');
+		            return originX && originY && originX === originY ? originX : '50%';
+		        }
+		    }
+		};
+	}).call();
+
+	function isString(str) {
+		return (Object.prototype.toString.call(str) == '[object String]');
+	}
+
+    // creates a new transform description from base transform description 
+	function createLikeTransWithBaseValuedParams(sourceTrans) {
+	    var parameters = [],
+            baseNumber = 0;
+		
+	    if (sourceTrans.name.substring(0, 5) == 'scale') baseNumber = 1;
+
+		for (var i = 0, il = sourceTrans.parameters.length; i < il; i++) {
+		    parameters.push({ text: 0, number: baseNumber });
+		}
+
+		return new TransformDescription(sourceTrans.name, parameters);
+		//{
+		//	name: sourceTrans.name,
+		//	parameters: parameters
+		//};
+	}
+    
+	// creates a regular expression to match a transform of a specific name
+	// !uses caches re so not for re.exec!
+	function createTransformRe(name) {
+		var re;
+
+		if (!(re = nameTransformCache[name])) {
+			re = new RegExp(name + "\\s*\\(([^)]*)\\)", "gi");
+		}
+
+		return re;
+	}
+
+	// parameter on transform
+	var TransformParameter = function (number, unit) {
+		this.number = Number(number);
+		this.unit = unit || '';
+	};
+
+	(function () {
+		this.number = 0;
+		this.unit = '';
+
+		this.translate = function (param) {
+			return this.number += param.number;
+		};
+
+		this.toString = function () {
+			return this.number + this.unit;
+		};
+	}).call(TransformParameter.prototype);
+
+	function createParameterArray(parameterStr) {
+		var match,
+        parameters = [];
+		reNumberUnit = /([\-+]?\d+(?:\.\d+)?)([^,\s)]+)?/g; ///(\d+(?:\.\d+)?)(%|\w+)?/g;
+
+		while (match = reNumberUnit.exec(parameterStr)) {
+			parameters.push(new TransformParameter(match[1], match[2]));
+		}
+
+		return parameters;
+	}
+	// single transform
+	var TransformDescription = function (name, parameters) {
+		this.name = name;
+		this.parameters = (!Array.isArray(parameters)) ? createParameterArray(parameters) : parameters;
+	};
+
+	(function () {
+		this.name = '';
+		this.parameters = [];
+
+		this.translate = function (transParams) {
+			for (var i = 0, il = Math.min(this.parameters.length, transParams.length) ; i < il; i++) {
+				this.parameters[i].translate(transParams[i]);
+			}
+		};
+		this.toString = function () {
+			return this.name + '(' + this.parameters.join(', ') + ')';
+		};
+	}).call(TransformDescription.prototype);
+
+	// transforms on an element
+	var ElementTransforms = function (str) {
+		this.transforms = [];
+		this.names = [];
+
+		if (str) {
+			var match, name, parameters;
+
+			while (match = reFindTransforms.exec(str)) {
+				this.add(match[1], match[2]);
+			}
+		}
+	};
+
+	(function () {
+		this.transforms = [];
+		this.names = [];
+
+		this.add = function (name, parameters) {
+			this.transforms.push(new TransformDescription(name, parameters));
+			this.names.push(name);
+		};
+
+		this.getByName = function (name) {
+			return this.transforms[this.names.indexOf(name)];
+		};
+
+		this.contains = function (name) {
+			return this.names.indexOf(name);
+		};
+
+		this.toString = function () {
+			return this.transforms.join(' ');
+		};
+	}).call(ElementTransforms.prototype);
+
+	function get(elem) {
+		//return window.getComputedStyle(elem, null).getPropertyValue(cssTransformKey);
+		return elem.style[styleTransformKey];
+	}
+	function getParameters(elem, transformName) {
+	    //return window.getComputedStyle(elem, null).getPropertyValue(cssTransformKey);
+	    var re = createTransformRe(transformName),
+            match;
+
+	    if ((match = re.exec(get(elem)))) {
+	        return createParameterArray(match[1]);
+	    }
+
+	    return null;
+
+	}
+	function set(elem, value) {
+
+		return elem.style[styleTransformKey] = value;
+	}
+
+	function _updateFromElementTransforms(elem, elemTrans) {
+		var transformText = get(elem).replace('none', ''),
+			transform, propKey;
+
+		for (var i = 0, il = elemTrans.transforms.length; i < il; i++) {
+			transform = elemTrans.transforms[i];
+
+		    // add properties support
+			if (!(propKey = propKeys[transform.name])) {
+			    if (transformText.indexOf(transform.name) > -1) {
+			        transformText = transformText.replace(createTransformRe(transform.name), transform.toString());
+			    } else {
+			        transformText += ' ' + transform.toString();
+			    }
+			} else {
+			    $(elem).css(propKey, transform.parameters.join(' '));
+			}
+		}
+
+		set(elem, transformText);
+
+		return this;
+	}
+
+	function _updateFromString(elem, str) {
+		var match,
+			transformText = get(elem).replace('none', ''),
+			tranName, propKey;
+
+		while (match = reFindTransforms.exec(str)) {
+		    tranName = match[1];
+		    if (!(propKey = propKeys[tranName])) {
+		        if (transformText.indexOf(tranName) > -1) {
+		            transformText = transformText.replace(createTransformRe(tranName), match);
+		        } else {
+		            transformText += ' ' + match[0];
+		        }
+		    }
+		    else {
+		        $(elem).css(propKey, match[1]);
+		    }
+		}
+		set(elem, transformText);
+
+		return this;
+	}
+
+
+	function animationSetup(elem, transformStr, options, animations) {
+		if (!options) options = {};
+
+		var match, thisTrans, thisItem, animateTrans, animItem, mode, propKey
+            animationDescriptions = {
+				items: []
+			};
+
+		thisTrans = new ElementTransforms(get(elem));
+		animateTrans = new ElementTransforms(transformStr);
+
+		for (var i = 0, il = animateTrans.transforms.length; i < il; i++) {
+		    animItem = animateTrans.transforms[i];
+		    if (!(propKey = propKeys[animItem.name])) {
+		        if (!(thisItem = thisTrans.getByName(animItem.name))) {
+
+		            thisItem = createLikeTransWithBaseValuedParams(animItem);
+		        }
+		    } else {
+		        thisItem = new TransformDescription(animItem.name, elem.style[propKey]);
+		    }
+			
+			if (options.mode == 'rel') animItem.translate(thisItem.parameters);
+
+			animationDescriptions.items.push({
+				name: animItem.name,
+				start: thisItem.parameters,
+				end: animItem.parameters,
+			});
+		}
+
+		if (!animations) animations = {};
+		animations['_dg.transimate'] = 0;
+
+		$(elem).data('_dg.transimate', animationDescriptions).animate(animations, options);
+
+	}
+
+	function animationStep(now, fx) {
+	    
+		if (fx.prop === "_dg.transimate") {
+			var 
+				desc = $(this).data('_dg.transimate'),
+				transUpdates = new ElementTransforms(),
+				paramUpdates, trans, startParam, endParam;
+
+			for (var i = 0, il = desc.items.length; i < il; i++) {
+				paramUpdates = [];
+				trans = desc.items[i];
+
+				// update parameters
+				for (var j = 0, jl = trans.end.length; j < jl; j++) {
+					
+					startParam = trans.start[j];
+					endParam = trans.end[j];
+
+					paramUpdates.push(new TransformParameter(
+						startParam.number + (endParam.number - startParam.number) * fx.pos,
+						endParam.unit));
+					
+				}
+
+				transUpdates.add(trans.name, paramUpdates);
+			}
+
+			_updateFromElementTransforms(this, transUpdates);
+		}
+	}
+	$.fn.transimate = function (method) {
+		var jq = this;
+
+		function jqGet(transformName) {
+		    if (jq.length == 0) return null;
+		    if (!transformName) {
+		        return get(jq[0]);
+		    }
+		    else {
+		        return getParameters(jq[0], transformName);
+		    }
+		}
+
+		function jqSet(value) {
+	
+			return jq.each(function() { return set(this, value); });
+		}
+
+		function val(value) {
+			if (value !== undefined) return jqSet($jq, value);
+			return jqGet(jq);
+		}
+
+		function contains(name) {
+			return createTransformRe(name).test(jq.css(cssTransformKey));
+		}
+
+		// updates transforms with transform[s] from string.  
+		// Replaces existing transforms, adds new ones to the end
+		function update(transforms) {
+			var updateMethod = (transforms.constructor == ElementTransforms) ?
+				_updateFromElementTransforms : _updateFromString;
+
+			return jq.each(function () { updateMethod(this, transforms) });
+		}
+		function animate(transforms, options) {
+			if (!options) options = {};
+			options.step = animationStep;
+
+			return jq.each(function () { animationSetup(this, transforms, options) });
+		}
+		switch (method) {
+		    case 'get': return jqGet(arguments[1]);
+			case 'set': return jqSet(arguments[1]);
+			case 'val': return val(arguments[1]);
+			case 'contains': return contains(arguments[1]);
+			case 'update': return update(arguments[1]);
+			case 'animate': return animate(arguments[1], arguments[2], arguments[3]);
+		    default: { 
+		        if (arguments.length == 1) {
+		            return jqSet(arguments[0]);
+		        } else {
+		            return animate(arguments[0], arguments[1], arguments[2]);
+		        }
+		    }
+		}
+	};
+
+}(jQuery));
