48dd560b4bda66187286d2202b33e4995c8f4755
Daniel Gimenez <danny@gimenez.biz>
2013-07-26 12:12:37 -0400

    Simple Tree
    changed template key name

--- DDtMM.Scripts/Scripts/Plugins/dgSimpleTree-1.0.4.js
+++ DDtMM.Scripts/Scripts/Plugins/dgSimpleTree-1.0.4.js
@@ -118,7 +118,7 @@ $.widget("dg.simpleTree", {
         if (typeof value == 'object') {
             for (dataID in value) {
                 $labelElement.find('[name="' + dataID + '"]').attr(dataID, value[dataID]);
-                $labelElement.find('[data-dataTemplate-ID="' + dataID + '"').html(value[dataID]);
+                $labelElement.find('[data-tree-key="' + dataID + '"').html(value[dataID]);
             }
         } else {
             $labelElement.html(value);
